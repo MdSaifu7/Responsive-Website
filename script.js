0a3360bee3db759e086f9010708dc22fb5e54c47
Saifu <mdsaifkhanahmad@gmail.com>
2024-05-18 14:16:13 +0530

    links updated

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const linkContainer=document.querySelector('.linkContainer')
 const allLInks= document.querySelectorAll('a')
 
 
-allLInks.forEach(a=>a.addEventListener('mouseenter',()=>{a.classList.add('active')}))
-allLInks.forEach(a=>a.addEventListener('mouseleave',()=>{a.classList.remove('active')}))
+allLInks.forEach(a=>a.addEventListener('mouseenter',()=>{a.parentElement.classList.add('active')}))
+allLInks.forEach(a=>a.addEventListener('mouseleave',()=>{a.parentElement.classList.remove('active')}))
